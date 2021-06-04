f93f82ecdbd689488e150ea5540f9cf24d8c56d1
HUNcamper <selimnahimi@gmail.com>
2021-06-04 23:46:49 +0200

    IsEnabled not found on clientside

--- goldsrc_footsteps/lua/autorun/client/cl_cfootsteps.lua
+++ goldsrc_footsteps/lua/autorun/client/cl_cfootsteps.lua
@@ -6,10 +6,11 @@
 -- This is required, since otherwise the client keeps trying
 -- to play the original sounds next to the custom ones.
 
-hook.Add( "PlayerFootstep", "CustomFootstep", function( ply, pos, foot, sound, volume, rf )
-	if (IsEnabled()) then return true end -- Return true disables anything default
-end)
-
 local function IsEnabled()
 	return GetConVar("gsrc_footsteps_enabled"):GetBool()
 end
+
+
+hook.Add( "PlayerFootstep", "CustomFootstep", function( ply, pos, foot, sound, volume, rf )
+	if (IsEnabled()) then return true end -- Return true disables anything default
+end)
